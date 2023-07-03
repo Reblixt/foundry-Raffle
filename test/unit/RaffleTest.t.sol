e90f5773d66ef454c072f600606daf659c228cd9
Reblixt <carl@klofverskjold.se>
2023-07-03 22:30:59 +0200

    chore: forge init

--- test/unit/RaffleTest.t.sol
+++ test/unit/RaffleTest.t.sol
@@ -4,5 +4,6 @@ pragma solidity ^0.8.19;
 
 import {DeployRaffle} from "../../script/DeployRaffle.s.sol";
 import {Raffle} from "../../src/Raffle.sol";
+import {Test, console} from "../../lib/forge-std/src/Test.sol";
 
 contract RaffleTest {}
